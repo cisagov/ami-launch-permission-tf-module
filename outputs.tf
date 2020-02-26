8c1e866c26e0663257374fc53f52e568c827bc7a
Dave Redmin <david.redmin@beta.dhs.gov>
2020-02-25 22:56:49 -0500

    Define output

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,4 @@
-output "id" {
-  value       = aws_instance.example.id
-  description = "The EC2 instance ID"
-}
-
-output "arn" {
-  value       = aws_instance.example.arn
-  description = "The EC2 instance ARN"
-}
-
-output "availability_zone" {
-  value       = aws_instance.example.availability_zone
-  description = "The AZ where the EC2 instance is deployed"
-}
-
-output "private_ip" {
-  value       = aws_instance.example.private_ip
-  description = "The private IP of the EC2 instance"
-}
-
-output "subnet_id" {
-  value       = aws_instance.example.subnet_id
-  description = "The ID of the subnet where the EC2 instance is deployed"
+output "accounts" {
+  value       = aws_ami_launch_permission.accounts
+  description = "A map whose keys are the account names allowed to launch the AMI and whose values are the account IDs and the AMI ID."
 }
