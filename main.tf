8064bc0355ce50846d167e5e606d4d4f8f96536d
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-10-04 11:00:51 -0400

    Add the ability to specify extra-organizational acct IDs that can launch the AMI
    Also make the default value for the regex "^$", which should not match
    any valid account name.  This allows that value to be optional, so one
    can use the module to only create launch permissions for
    extra-organizational account IDs.

--- main.tf
+++ main.tf
@@ -4,16 +4,18 @@ data "aws_organizations_organization" "org" {
 }
 
 # ------------------------------------------------------------------------------
-# Add launch permissions for all non-master accounts in the organization
-# whose names match the account_name_regex.
+# Add launch permissions for all non-master accounts in the
+# organization whose names match the account_name_regex, as well as
+# any extra-organizational account IDs listed in
+# var.extraorg-account_ids.
 # ------------------------------------------------------------------------------
 
 resource "aws_ami_launch_permission" "accounts" {
-  for_each = {
+  for_each = toset(distinct(concat([
     for account in data.aws_organizations_organization.org.non_master_accounts :
-    account.name => account.id
+    account.id
     if length(regexall(var.account_name_regex, account.name)) > 0
-  }
+  ], var.extraorg_account_ids)))
 
   image_id   = var.ami_id
   account_id = each.value
